97ebec55f21be6721fd4e2fae64000d91c6ba5f4
Sitaram Oli <sitaramoli1998@gmail.com>
2025-06-28 19:31:32 -0400

    Add: OG Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,33 @@ import Footer from '@/components/layout/Footer';
 import {portfolioData} from '@/config/portfolio-data';
 import React from "react";
 import Head from "next/head";
-import { Analytics } from "@vercel/analytics/next"
+import {Analytics} from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: `${portfolioData.name} - ${portfolioData.jobTitle}`,
     description: portfolioData.hero.shortBio,
+    openGraph: {
+        title: `${portfolioData.name} - ${portfolioData.jobTitle}`,
+        description: portfolioData.hero.shortBio,
+        url: "https://sitaramoli.com.np",
+        type: "website",
+        images: [
+            {
+                url: "https://opengraph.b-cdn.net/production/images/b1305d0b-6fbd-4d32-b8cd-3daddc020632.jpg?token=qz9qFMJrxMiv1ayjKTsRmBrMAmzxiSlFR7LyG-1vtLg&height=800&width=1200&expires=33287152517",
+                width: 1200,
+                height: 800,
+                alt: portfolioData.hero.shortBio,
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: `${portfolioData.name} - ${portfolioData.jobTitle}`,
+        description: `${portfolioData.name} - ${portfolioData.jobTitle}`,
+        images: [
+            "https://opengraph.b-cdn.net/production/images/b1305d0b-6fbd-4d32-b8cd-3daddc020632.jpg?token=qz9qFMJrxMiv1ayjKTsRmBrMAmzxiSlFR7LyG-1vtLg&height=800&width=1200&expires=33287152517",
+        ],
+    },
 };
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
